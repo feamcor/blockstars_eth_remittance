c2a054aff21631cf898f03a82af1f61e183ec0d5
Fabio Correa <feamcor@gmail.com>
2019-05-08 06:48:01 -0400

    Add 0.05 ETH as initial fee

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 const Remittance = artifacts.require("Remittance");
+const { toBN, toWei } = web3.utils;
 
 module.exports = function(deployer) {
-  deployer.deploy(Remittance);
+  deployer.deploy(Remittance, toBN(toWei("0.05", "ether")));
 };
